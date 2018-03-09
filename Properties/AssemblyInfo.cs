441cd7b8851535af3571f16d5a6adecf72605c9b
Utku Garip <utku.garip@gmail.com>
2018-03-09 17:40:32 +0300

    UtkuGARIP tarafından Türkçe Dil Eklenmiştir
    UtkuGARIP tarafından Türkçe Dil Eklenmiştir

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,39 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MapMakerTools")]
+// Bir assembler hakkında genel bilgi, aşağıdaki özellikler dizisi 
+// aracılığıyla kontrol edilir. Bir assembler ile ilişkili bilgileri 
+// değiştirmek için bu özellik değerlerini değiştirin.
+[assembly: AssemblyTitle("Harita-Üreticisi-Aracı")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MapMakerTools")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Harita-Üreticisi-Aracı")]
+[assembly: AssemblyCopyright("Telif Hakkı ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyTranslator("UtkuGARIP")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// ComVisible ayarı, bu derlemedeki türleri COM bileşenlerine 
+// görünmez yapar.  Bu derlemede COM türünden bir türe erişmeniz 
+// gerekiyorsa, ComVisible özniteliğini bu türde 
+// true olarak ayarlayın.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// Bu proje COM'a maruz kalıyorsa, aşağıdaki GUID typelib kimliği içindir.
 [assembly: Guid("29247871-636d-4dc4-abed-0810c1dca99d")]
 
-// Version information for an assembly consists of the following four values:
+// Bir derleme için sürüm bilgisi aşağıdaki dört değerden oluşur:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Derleme Numarası
+//      Düzeltilmiş
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Tüm değerleri belirtebilir veya aşağıdaki gibi '*' kullanarak 
+// Yapı ve düzenleme numaralarını varsayılan yapabilirsiniz:
+// Örneğin: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.2")]
 [assembly: AssemblyFileVersion("1.0.2")]
